0e8043811f94d6116d5989030feb7e60e50530df
Yuset Calzadilla <yuset.calzadilla@gmail.com>
2020-03-26 08:24:44 -0500

    remove unused files

--- infrastructure/installer/test/fixtures/infrastructure/bamboo/build/build-container/build-docker-container.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-rm docker-compose.override.yaml
-
-sudo -E BRANCH=$BRANCHNAME docker-compose build
--- infrastructure/installer/test/fixtures/infrastructure/bamboo/build/install-dependencies/tar-compress.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-tar --exclude="./infrastructure" --exclude='./infrastructure' -czvf ./sourcecode.tar.gz app
-tar -czvf ./infrastructure.tar.gz infrastructure docker-compose.yaml docker-compose.override.yaml stack.testing.yaml
